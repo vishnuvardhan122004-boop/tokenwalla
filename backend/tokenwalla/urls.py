55c671c9638f118c53d3787aae3dc6bdf3c126f6
vishnu <vishnu.vy143@gmail.com>
2026-03-22 10:21:18 +0530

    fix urls - accounts -> users

--- backend/tokenwalla/urls.py
+++ backend/tokenwalla/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/',         admin.site.urls),
-    path('api/auth/',      include('accounts.urls')),
+    path('api/auth/',      include('users.urls')),
     path('api/hospitals/', include('hospitals.urls')),
     path('api/doctors/',   include('doctors.urls')),
     path('api/bookings/',  include('bookings.urls')),
